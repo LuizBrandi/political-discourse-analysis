f1c69323ebf42afc4e70cac304c5cec3e3a92b9e
gpmoura4 <gpmoura4@gmail.com>
2026-04-11 21:00:06 -0300

    similaridade discurso com pauta

--- src/discourses/_00_main.py
+++ src/discourses/_00_main.py
@@ -1,17 +1,9 @@
 
-<<<<<<< HEAD
-from _01_list_extract import list_extract
-from _02_discourses_extract import discourses_extract
-from _03_discourse_preprocessing import preprocessing
-from _04_topics import topics_main
-from _05_embeddings import generate_discourse_embeddings
-=======
 from src.discourses._01_list_extract import list_extract
 from src.discourses._02_discourses_extract import discourses_extract
 from src.discourses._03_discourse_preprocessing import preprocessing
 from src.discourses._04_topics import topics_main
 from src.discourses._05_embeddings import generate_discourse_embeddings
->>>>>>> 4116edb824895ebfa71ebc659603af6ff5f40d7b
 # from _05_llm_analysis import llm_analysis
 
 import pandas as pd
@@ -30,7 +22,7 @@ SOURCE_CSV_NAME = "political_discourses_ini_02072022_fim_29102022.csv"
 df, name = preprocessing ( *discourses_extract ( *list_extract("09/05/2025", "09/09/2025") ) )
 
 # # ETAPA 4 para o partido selecionado
-topics_main (df, "UNIÃO")
+topics_main (df, "UNIAO")
 
 # ETAPA 5 para um partido específico
 embeddings_df, embeddings_matrix, base_name = generate_discourse_embeddings(df, party="UNIÃO", source_csv_name=SOURCE_CSV_NAME)
