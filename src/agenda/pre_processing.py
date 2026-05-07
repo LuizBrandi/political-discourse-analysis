017321b880fe6ecbe139cb1a3cb9e14e9718ce48
gpmoura4 <gpmoura4@gmail.com>
2026-05-07 10:39:12 -0300

    data tokens v21

--- src/agenda/pre_processing.py
+++ src/agenda/pre_processing.py
@@ -11,7 +11,7 @@ from typing import Any
 import spacy
 from nltk.corpus import stopwords
 
-from .embeddings import DEFAULT_MODEL_NAME, _load_sentence_transformer, segment_text_semantic
+from agenda.embeddings import DEFAULT_MODEL_NAME, _load_sentence_transformer, segment_text_semantic
 
 
 STOPWORDS_ADICIONAIS = [
